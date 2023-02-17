8c78c85b89904ef217b286171c32374112de47ed
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-02-17 15:07:01 +0530

    Dashboard done

--- dashboard/src/Components/Dashboard.jsx
+++ dashboard/src/Components/Dashboard.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Input from './Input'
 import List from './List'
+import { useDispatch } from 'react-redux'
+import { getTodos } from '../Redux/action'
+
 
 const Dashboard = () => {
+    const dispatch = useDispatch();
+  useEffect(()=>{
+    getTodos(dispatch);
+  },[dispatch])
   return (
     <div>
         <Input/>
--- dashboard/src/Components/List.jsx
+++ dashboard/src/Components/List.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { useSelector } from "react-redux"
 
 const List = () => {
-    const todos=useSelector(state=>state.todos)
-    console.log(todos)
+    const todos=useSelector(state=>state.todos);
   return (
     <div>
       <h3>Todos</h3>
